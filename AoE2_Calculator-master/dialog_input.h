25b7a4f626d4c02983efb6ce4bd49690a4aa0131
Phillip Schackier <CppPhil@users.noreply.github.com>
2023-10-22 15:28:03 +0200

    Update

--- AoE2_Calculator-master/dialog_input.h
+++ AoE2_Calculator-master/dialog_input.h
@@ -4,6 +4,7 @@
 
 #include <memory>
 #include <sstream>
+#include <type_traits>
 
 #include "mainwindow.hpp"
 
@@ -14,29 +15,71 @@ public:
   template<typename Any>
   friend DialogInput& operator>>(DialogInput& din, Any& any)
   {
-    // TODO: Funny business to detect the type of any.
-
+    using UnCvRef = std::remove_cv_t<std::remove_reference_t<Any>>;
     bool          ok{};
-    const QString userInput = QInputDialog::getText(
-      /* parent */ din.m_mainWindow,
-      /* title */ "Data entry prompt",
-      /* label */ "PLACEHOLDER",
-      /* mode */ QLineEdit::Normal,
-      /* text */ QString{},
-      /* ok */ &ok);
+    const QString title{"Data entry prompt"};
+    const QString label{"PLACEHOLDER"};
 
-    if (!ok) {
-      qFatal() << "Couldn't get text from user in DialogInput::operator>>";
+    if constexpr (std::is_same_v<UnCvRef, double>) {
+      const double result{QInputDialog::getDouble(
+        /* parent */ din.m_mainWindow,
+        /* title */ title,
+        /* label */ label,
+        /* value */ 0.0,
+        /* min */ -2147483647,
+        /* max */ 2147483647,
+        /* decimals */ 1,
+        /* ok */ &ok)};
+
+      if (!ok) {
+        qFatal() << "Couldn't get double from user in DialogInput::operator>>";
+        return din;
+      }
+
+      any = result;
       return din;
     }
+    else if (std::is_same_v<UnCvRef, int>) {
+      const int result{QInputDialog::getInt(
+        /* parent */ din.m_mainWindow,
+        /* title */ title,
+        /* label */ label,
+        /* value */ 0,
+        /* min */ -2147483647,
+        /* max */ 2147483647,
+        /* sept */ 1,
+        /* ok */ &ok)};
 
-    std::istringstream iss{userInput.toStdString()};
-    if (!(iss >> any)) {
-      qFatal() << "Couldn't write to variable in DialogInput::operator>>";
+      if (!ok) {
+        qFatal() << "Couldn't get int from user in DialogInput::operator>>";
+        return din;
+      }
+
+      any = result;
       return din;
     }
+    else {
+      const QString userInput = QInputDialog::getText(
+        /* parent */ din.m_mainWindow,
+        /* title */ title,
+        /* label */ label,
+        /* mode */ QLineEdit::Normal,
+        /* text */ QString{},
+        /* ok */ &ok);
 
-    return din;
+      if (!ok) {
+        qFatal() << "Couldn't get text from user in DialogInput::operator>>";
+        return din;
+      }
+
+      std::istringstream iss{userInput.toStdString()};
+      if (!(iss >> any)) {
+        qFatal() << "Couldn't write to variable in DialogInput::operator>>";
+        return din;
+      }
+
+      return din;
+    }
   }
 
   explicit DialogInput(MainWindow* mainWindow);
